a1ab5dda84fe7dacc127ce0889654debe4078790
TheZoq2 <frans.skarman@gmail.com>
2018-05-22 12:06:23 +0200

    Make http server load page at runtime

--- host/src/httpserver.rs
+++ host/src/httpserver.rs
@@ -1,5 +1,8 @@
 use simple_server::Server;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn http_server() {
     let host = "127.0.0.1";
     let port = "7878";
@@ -9,8 +12,17 @@ pub fn http_server() {
     let server = Server::new(|request, mut response| {
         println!("Request received. {} {}", request.method(), request.uri());
 
-        let file = include_str!("../frontend/index.html");
-        Ok(response.body(file.as_bytes())?)
+        // let file = include_str!("../frontend/index.html");
+        let content = File::open("frontend/index.html")
+            .and_then(|mut file| {
+                let mut content = String::new();
+                file.read_to_string(&mut content)?;
+
+                Ok(content)
+            })
+            .unwrap_or_else(|err| format!("{:#?}", err));
+
+        Ok(response.body(content.into_bytes())?)
     });
 
     println!("Http server listening on on http://localhost:{}", port);
