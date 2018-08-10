c1db282cf48318ec2b7e526005bdb7b80e45816d
TheZoq2 <frans.skarman@gmail.com>
2018-08-10 20:25:48 +0200

    Add function for measuring the gap between high and low

--- host/frontend/src/Signal.elm
+++ host/frontend/src/Signal.elm
@@ -53,3 +53,38 @@ edgeTrigger trigFn valueRange readings =
         end = min (lastTrig + valueRange/2) maxTime
     in
         (end - valueRange, end)
+
+
+{-|
+  Finds the length of the gap at `gapLocation` if gapLocation
+  is larger than the max time in the list `Nothing` is returned
+-}
+gapLength : Float -> List (Float, Bool) -> Maybe Float
+gapLength gapLocation values =
+    let
+        -- Finds the last value before location along with when
+        -- that value was set
+        recurseFn : (Bool, Float) -> List (Float, Bool) -> Maybe Float
+        recurseFn (lastValue, lastChange) values =
+            case values of
+                (time, value) :: rest ->
+                    -- If the last value is the same as the current value,
+                    -- do nothing
+                    if lastValue == value then
+                        recurseFn (value, time) rest
+                    -- Otherwise, check if the time is up, if so return
+                    -- the interval length, otherwise recurse
+                    else
+                        if time > gapLocation then
+                            Just (time - lastChange)
+                        else
+                            recurseFn (lastValue, lastChange) rest
+                _ ->
+                    Nothing
+    in
+        case values of
+            (time, value) :: rest ->
+                recurseFn (value, 0) rest
+            _ ->
+                Nothing
+
